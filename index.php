4115bde3f0dc394a66eb1c09c3e18bf643054520
Solivanka <99730617+Solivanka@users.noreply.github.com>
2022-11-16 09:06:02 +0100

    data

--- index.php
+++ index.php
@@ -91,20 +91,18 @@ function test_input($data) {
 </form>
 
 
-<?php
-    if (isset($_POST['s1'])) {
-      echo $_POST['name'];
-      echo $_POST['email'];
 
-      $myfile = fopen("data.txt", "w") or die("Unable to open file");
-      $name = $_POST['name']."\n";
-      fwrite($myfile, $name);
+if (isset($_POST['s1'])) {
+    echo $_POST['name'];
+    echo $_POST['email'];
+    $myfile = fopen("data.txt", "w") or die("Unable to open file");
+    $name = $_POST['name']."\n";
+    fwrite($myfile, $name);
 
-      $email = $_POST['email']."\n";
-      fwrite($myfile, $email);
-      fclose($myfile);
-    }
-  ?>
+    $email = $_POST['email']."\n";
+    fwrite($myfile, $email);
+    fclose($myfile);
+}
 
 
 <?php
