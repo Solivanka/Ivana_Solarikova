acf5f698122fb4082f7055aa47622d2c07916a63
Solivanka <99730617+Solivanka@users.noreply.github.com>
2022-11-17 12:33:58 +0100

    a

--- projekt/index.php
+++ projekt/index.php
@@ -1,35 +1,11 @@
-<!DOCTYPE html>
 <html>
-<head>
+<body>
 
-<h2>Webový formulár</h2>
-
-<form action="index.php" method="post">  
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <br><br>
-  Surname: <input type="text" name="surname" value="<?php echo $surname;?>">
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <br><br>
-  Gender: <input type="text" name="gender" value="<?php echo $website;?>">
-  <br><br>
-  Age: <input type="text" name="age" value="<?php echo $name;?>">
-  <br><br>
-  Hometown: <input type="text" name="hometown" value="<?php echo $surname;?>">
-  <br><br>
-  <input type="submit" name="save" value="Save data in text file">
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
 </form>
 
 </body>
 </html>
-
-<?php
-              
-if(isset($_POST['surname']))
-{
-$data=$_POST['surname'];
-$fp = fopen('data.txt', 'a');
-fwrite($fp, $data);
-fclose($fp);
-}
-?>
