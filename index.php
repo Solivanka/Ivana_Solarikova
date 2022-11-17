8627b56eea0691249558e4447b8ad9c0abb9ecfb
Solivanka <99730617+Solivanka@users.noreply.github.com>
2022-11-17 12:52:15 +0100

    a

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 
 <form action="index.php" method="post">
 Name: <input type="text" name="name"><br>
+Surname: <input type="text" name="surname"><br>
+Age: <input type="text" name="age"><br>
+Hometown: <input type="text" name="hometown"><br>
+School: <input type="text" name="school"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
