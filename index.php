bc3f3689fda762ee02ae4ec941738b71fa19b055
Solivanka <99730617+Solivanka@users.noreply.github.com>
2023-02-02 14:21:05 +0100

    a

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ function clickCounter() {
 function resetCounter() {
   if (typeof(Storage) !== "undefined") {
     if (localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickcount)+1;
+      localStorage.clickcount = Reset(localStorage.clickcount)+1;
     } else {
       localStorage.clickcount = 1;
     }
