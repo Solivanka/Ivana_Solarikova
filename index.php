ee15d42e8121704dbc08e4e44dcd11f0a4b0f678
Solivanka <99730617+Solivanka@users.noreply.github.com>
2023-02-02 14:13:31 +0100

    a

--- index.php
+++ index.php
@@ -1,41 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head>
-<script>
-function clickCounter() {
-  if (typeof(Storage) !== "undefined") {
-    if (sessionStorage.clickcount) {
-      sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
-    } else {
-      sessionStorage.clickcount = 1;
-    }
-    document.getElementById("result1").innerHTML = "You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
-  } else {
-    document.getElementById("result1").innerHTML = "Sorry, your browser does not support web storage...";
-  }
-}
-
-function resetCounter() {
-  if (typeof(Storage) !== "undefined") {
-    if (sessionStorage.clickcount) {
-      sessionStorage.clickcount = Reset(sessionStorage.clickcount)+1;
-    } else {
-      sessionStorage.clickcount = 1;
-    }
-    document.getElementById("result2").innerHTML = "You have reset the button " + sessionStorage.clickcount + " time(s) in this session.";
-  } else {
-    document.getElementById("result2").innerHTML = "Sorry, your browser does not support web storage...";
-  }
-}
-
-</script>
-</head>
 <body>
+<form method="post">
 
-<p><button onclick="clickCounter()" type="button">Click me!</button></p>
-<div id="result1"></div>
-<p><button onclick="resetCounter()" type="button">Reset!</button></p>
-<div id="result2"></div>
+<button type="submit" name="submit">Send</button>
+<input type="reset" value="Reset" class="reset_btn" name="reset">
 
+<?php
+
+$button = 2;
+    if(isset($_POST["submit"]))
+    {
+        $button++;
+    }
+    if(isset($_POST["reset"]))
+    {
+        $button = 0;
+    }
+    echo $button;
+?>
+
+</form>
 </body>
 </html>
