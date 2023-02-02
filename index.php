ade5e765897da4eb28ae006e461ed319398dd00f
Solivanka <99730617+Solivanka@users.noreply.github.com>
2023-02-02 13:10:35 +0100

    a

--- index.php
+++ index.php
@@ -3,22 +3,19 @@
 <head>
 <script>
 function clickCounter() {
-  if (typeof(Storage) !== "undefined") {
     if (localStorage.clickcount) {
       localStorage.clickcount = Number(localStorage.clickcount)+1;
     } else {
       localStorage.clickcount = 1;
     }
-    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
-  } else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    document.getElementById("result").innerHTML = "Tlacitko si stlacil " + localStorage.clickcount + " krat.";
   }
 }
 </script>
 </head>
 <body>
 
-<p><button onclick="clickCounter()" type="button">Click me!</button></p>
+<p><button onclick="clickCounter()" type="button">Klikni!</button></p>
 <div id="result"></div>
 <p>Click the button to see the counter increase.</p>
 <p>Close the browser tab (or window), and try again, and the counter will continue to count (is not reset).</p>
